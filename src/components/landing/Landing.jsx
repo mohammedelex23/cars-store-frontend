1b44e4a01d77f8b02dfab402bc8d3cf1b8821c8f
mohammed ismail <warethco@gmail.com>
2023-05-21 21:54:34 +0200

    some work on landing

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,5 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareArrowUpRight } from "@fortawesome/free-solid-svg-icons";
 import "./Landing.css";
 const Landing = () => {
-  return <section className="landing"></section>;
+  return (
+    <section className="landing">
+      <h3 className="title">Find the perfect car for you at YourCar.</h3>
+      <p className="desc">
+        We offer a wide range of cars that cater to your needs and budget. Visit
+        us today and drive away with your dream car!
+      </p>
+      <button className="btn">
+        <span>Discover</span>
+        <FontAwesomeIcon icon={faSquareArrowUpRight} />
+      </button>
+    </section>
+  );
 };
 export default Landing;
